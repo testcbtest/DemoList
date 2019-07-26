9751aa9104f588c902238917084c0eac000cd446
chaouki chaouachi <chaouki.chaouachi.rf@gmail.com>
2019-07-26 12:03:12 +0200

    Create ViewExtension.kt

---
+++ app/src/main/java/com/test/demo/ui/ViewExtension.kt
@@ -0,0 +1,15 @@
+package com.example.testandroidapplication.ui.adapters
+
+import android.content.ContextWrapper
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+fun View.getParentActivity() : AppCompatActivity? {
+    var context = this.context
+    while (context is ContextWrapper){
+        if(context is AppCompatActivity){
+            return context;
+        }
+    }
+    return null
+}
