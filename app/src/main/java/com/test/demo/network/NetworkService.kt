b6e9e7bdb41dd2f608af239ad829781d9d35f443
chaouki chaouachi <chaouki.chaouachi.rf@gmail.com>
2019-07-26 11:20:14 +0200

    add Dagger Dependencies

--- app/src/main/java/com/test/demo/network/NetworkService.kt
+++ app/src/main/java/com/test/demo/network/NetworkService.kt
@@ -1,6 +1,5 @@
 package com.example.testandroidapplication.network
 import com.example.testandroidapplication.model.Album
-import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.GET
 
