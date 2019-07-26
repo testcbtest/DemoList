e5990754dbb72da50efcc8395ccb75e4cdc6553a
chaouki chaouachi <chaouki.chaouachi.rf@gmail.com>
2019-07-26 11:05:17 +0200

    Create Album.kt
    Create Album Model

---
+++ app/src/main/java/com/test/demo/model/Album.kt
@@ -0,0 +1,12 @@
+package com.example.testandroidapplication.model
+
+/*
+Create Album model
+ */
+data class Album(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
